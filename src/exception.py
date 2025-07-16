03c7f13d65ad9b6afe0101b2996f2ab83a168864
siddhamapple <siddhamjainn@gmail.com>
2025-07-16 20:00:18 +0530

    Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() 
     #Traceback object shows exactly where your code failed in exc_info , 3rd element of tuple is traceback object]
@@ -14,10 +14,18 @@ def error_message_detail(error,error_detail:sys):
 
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_message_detail:sys):
-        super().__init__(error_message)
-        self.error_message= error_message_detail(error_message,error_detail=error_detail)
-
-
+    def __init__(self, error, error_detail: sys):
+        super().__init__(str(error))
+        self.error_message = error_message_detail(error, error_detail)
     def __str__(self):
         return self.error_message
+
+
+# if __name__ == "__main__":
+#     try:
+#         x=1/0
+    
+#     except Exception as ce:
+     
+#         logging.info("Divide by zero")
+#         raise CustomException(ce,sys)
--- src/logger.py
+++ src/logger.py
@@ -20,4 +20,4 @@ logging.basicConfig(
 
 
 if __name__=="__main__":
-    logging.info("Logging has started")
+    logging.log("Logging has started")
